289b34eb07fde468ca5e5b93f4a84a551fb698c0
Caique Luz <caaique.balotelli@gmail.com>
2022-05-26 18:28:50 -0300

    Teste commit

--- Teste_Ambiente/src/main/java/caique/luz/Teste_Ambiente/App.java
+++ Teste_Ambiente/src/main/java/caique/luz/Teste_Ambiente/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! Teste git" );
-    }
-}
+        System.out.println("Teste commit");
+        
+        
+        
+        ]
