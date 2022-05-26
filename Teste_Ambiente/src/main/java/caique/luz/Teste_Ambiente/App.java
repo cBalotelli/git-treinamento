2ae21561747c20903f0dfd485b7eb238e9a77376
Caique Luz <caaique.balotelli@gmail.com>
2022-05-26 18:19:30 -0300

    Teste_Ambiente_Git

---
+++ Teste_Ambiente/src/main/java/caique/luz/Teste_Ambiente/App.java
@@ -0,0 +1,13 @@
+package caique.luz.Teste_Ambiente;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Teste git" );
+    }
+}
