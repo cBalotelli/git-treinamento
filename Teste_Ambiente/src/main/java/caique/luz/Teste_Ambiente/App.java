fd6b2f7a529591cf57e884b20d527e7d128b88fc
Caique Luz <caaique.balotelli@gmail.com>
2022-05-27 10:18:10 -0300

    Teste commit 1.2

--- Teste_Ambiente/src/main/java/caique/luz/Teste_Ambiente/App.java
+++ Teste_Ambiente/src/main/java/caique/luz/Teste_Ambiente/App.java
@@ -12,5 +12,9 @@ public class App
         System.out.println("Teste commit");
         System.out.println("Teste commit");
         System.out.println("Teste commit");
+        System.out.println("Teste commit");
+        System.out.println("Teste commit");
+        System.out.println("Teste commit");
+        
         
     }
